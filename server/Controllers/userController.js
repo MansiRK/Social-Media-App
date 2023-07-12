0da74d7c8b9f299c6d294b52ac47ea0bde9c1904
MansiRK <19302009mansi@viva-technology.org>
2023-07-12 12:58:30 +0530

    added user part

---
+++ server/Controllers/userController.js
@@ -0,0 +1,14 @@
+const UserModel = require("../Models/userModel")
+
+const searchUser = async(req, res) =>{
+
+}
+
+
+const getUser = async(req, res) =>{
+    
+}
+
+const updateUser = async(req, res) =>{
+    
+}
