9d73f3f05b4d5878b57edd89ad9fe095f08e525d
Mansi Kamble <113781741+MansiRK@users.noreply.github.com>
2023-07-19 10:32:16 +0530

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -12,6 +12,7 @@ router.patch('/user', auth, userCtrl.updateUser)
 router.patch('/user/:id/follow', auth, userCtrl.follow)
 router.patch('/user/:id/unfollow', auth, userCtrl.unfollow)
 
+router.get('/suggestionsUser', auth, userCtrl.suggestionsUser)
 
 
 
