459b3dd9e316fd0500a9e24ec21ac51f302a317f
MansiRK <19302009mansi@viva-technology.org>
2023-07-31 15:08:12 +0530

    adding user model

---
+++ models/userModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        unique: false,
+        maxlength: 15
+    },
+    lastname: {
+        type: String,
+        required: true,
+        unique: false,
+        maxlength: 15
+    }
+})
--- server.js
+++ server.js
@@ -5,10 +5,14 @@ dotenv.config()
 
 const app = express()
 
+app.get("/", (req, res) => {
+    res.send("This is from server")
+})
+
 app.listen(process.env.PORT, (error) =>{
     if(error){
-        console.log("Server is unable to start due to", error)
+        console.log("Server unable to start due to error: ", error)
     }
 
-    console.log(`Server successfully started at PORT ${process.env.PORT}`)
+    console.log(`Server successfully started at PORT: ${process.env.PORT}`)
 } )
