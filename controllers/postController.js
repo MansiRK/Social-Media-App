d34bb69ae3d9b64c5174835f353f0c0a83ac4f67
MansiRK <19302009mansi@viva-technology.org>
2023-08-02 02:19:24 +0530

    done with like post

--- controllers/postController.js
+++ controllers/postController.js
@@ -197,23 +197,27 @@ const updatePost = async (req, res) => {
 
 const likePost = async (req, res) => {
   try {
+    // Find post
     const post = await postModel.findById({
       _id: req.params.id,
       likes: req.user._id,
     })
 
+    // If post is already liked
     if (post) {
       return res.status(400).json({
         message: "You have already liked this post.",
       })
     }
 
+    // Response when successful
     return res.status(200).json({
       message: "You successfully liked this post.",
       post,
     })
   }
   catch (error) {
+    // Response when error
     return res.status(500).json({
       message: `Failed to like this post. ${error.message}`,
     })
