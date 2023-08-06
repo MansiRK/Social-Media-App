90b25a89601271fa41e0559d04744f1df2464701
MansiRK <19302009mansi@viva-technology.org>
2023-08-07 01:29:39 +0530

    added fetch data url

--- client/utils/fetchData.js
+++ client/utils/fetchData.js
@@ -1,1 +1,93 @@
 import axios from "axios" 
+
+export const postAuthDataApi = async (url, token) => {
+    const res = await axios.post(`/api/auth/${url}`, {
+        headers: {
+            Authorization: token,
+        }
+    })
+    return res
+}
+
+export const getUserDataApi = async (url, token) => {
+    const res = await axios.get(`/api/user/${url}`, {
+        headers: {
+            Authorization: token,
+        }
+    })
+    return res
+}
+
+export const postUserDataApi = async (url, token) => {
+    const res = await axios.post(`/api/user/${url}`, {
+        headers: {
+            Authorization: token,
+        }
+    })
+    return res
+}
+
+export const patchUserDataApi = async (url, token) => {
+    const res = await axios.patch(`/api/user/${url}`, {
+        headers: {
+            Authorization: token,
+        }
+    })
+    return res
+}
+
+export const getPostDataApi = async (url, token) => {
+    const res = await axios.get(`/api/post/${url}`, {
+        headers: {
+            Authorization: token,
+        }
+    })
+    return res
+}
+
+export const postPostDataApi = async (url, token) => {
+    const res = await axios.post(`/api/post/${url}`, {
+        headers: {
+            Authorization: token,
+        }
+    })
+    return res
+}
+
+export const patchPostDataApi = async (url, token) => {
+    const res = await axios.patch(`/api/post/${url}`, {
+        headers: {
+            Authorization: token,
+        }
+    })
+    return res
+}
+
+export const deletePostDataApi = async (url, token) => {
+    const res = await axios.delete(`/api/post/${url}`, {
+        headers: {
+            Authorization: token,
+        }
+    })
+    return res
+}
+
+export const postCommentDataApi = async (url, token) => {
+    const res = await axios.post(`/api/comment/${url}`, {
+        headers: {
+            Authorization: token,
+        }
+    })
+    return res
+}
+
+export const patchCommentDataApi = async (url, token) => {
+    const res = await axios.patch(`/api/comment/${url}`, {
+        headers: {
+            Authorization: token,
+        }
+    })
+    return res
+}
+
+
